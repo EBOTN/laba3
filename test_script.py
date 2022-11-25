ceb7cd3a5fd82908b8b2efbd4202c0f8d6a49c8e
EBOTN <EBOTN@mail.ru>
2022-11-25 20:46:43 +0700

    second commit

--- test_script.py
+++ test_script.py
@@ -1,5 +1,5 @@
 from Python_lab_3 import *
 
 def test_calculate():
-    returnedArray = calculate(1000, 12, 10)
-    assert len(returnedArray) == 10
+    returnedArray = calculate(1000, 12, 5)
+    assert len(returnedArray) == 5
